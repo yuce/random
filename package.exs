fe4e4ec56fa43de9adf1259f8fd4be771f4f01eb
Yuce Tekol <yuce@gmail.com>
2013-09-01 02:06:56 +0300

    Added expm package

---
+++ package.exs
@@ -0,0 +1,14 @@
+Expm.Package.new(
+  name: "random",
+  description: "Elixir port of Python 3 random module.",
+  version: "0.0.2",
+  licenses: ["MIT"],
+  keywords: ["random", "math", "elixir"],
+  maintainers: [
+    [name: "Yuce Tekol", email: "yucetekol@gmail.com"]
+  ],
+  repositories: [
+    [git: "git@bitbucket.org:yuce/random.git"],
+    [github: "yuce/random"]
+  ]
+)
