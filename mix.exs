a7c9216e1dd77f3a6af5108ea6033883cb2f9bb6
Yuce Tekol <yucetekol@gmail.com>
2016-02-12 13:36:55 +0200

    updated mix

--- mix.exs
+++ mix.exs
@@ -6,7 +6,21 @@ defmodule Random.Mixfile do
       version: File.read!("VERSION") |> String.strip,
       elixir: ">= 1.1.0",
       name: "Random",
-      source_url: "https://bitbucket.org/yuce/random/"]
+      description: description,
+      package: package
+    ]
   end
 
+  defp description do
+    """
+    This module contains pseudo-random number generators for various distributions ported from Python 3 random module for Elixir.
+    """
+  end
+
+  defp package do
+    [maintainers: ["Yuce Tekol"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/yuce/random",
+              "Docs" => "http://yuce.me/random/"}]
+  end
 end
